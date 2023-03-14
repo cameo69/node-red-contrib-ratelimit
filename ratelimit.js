a92a854beae723ab53380453abb760a33bb754eb
cameo69 <44269764+cameo69@users.noreply.github.com>
2023-03-14 10:59:26 +0100

    formating

--- ratelimit.js
+++ ratelimit.js
@@ -1,42 +1,38 @@
 module.exports = function(RED) {
-    function RateLimitNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
+  function RateLimitNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
 
-        this.name = config.name;
-        this.messages = config.messages;
-        this.duration = config.duration;
-        this.addcurrentcount = true; //config.addcurrentcount;
-        this.msgcounter = 0;
+    this.name = config.name;
+    this.messages = config.messages;
+    this.duration = config.duration;
+    this.addcurrentcount = true; //config.addcurrentcount;
+    this.msgcounter = 0;
 
-        node.on('input', function(msg) {
-            
-            const NoOfMsg = node.messages;
-            const WindowInMilliSec = node.duration;
-            const AddCurrentCount = node.addcurrentcount;
+    node.on("input", function(msg) {
+      const NoOfMsg = node.messages;
+      const WindowInMilliSec = node.duration;
+      const AddCurrentCount = node.addcurrentcount;
 
-            function addTimeout() {
-                setTimeout(() => {
-                    let currentCount = node.msgcounter || 0;
-                    if (currentCount > 0) node.msgcounter = currentCount - 1;
-                }, WindowInMilliSec);
-            }
+      function addTimeout() {
+        setTimeout(() => {
+          let currentCount = node.msgcounter || 0;
+          if (currentCount > 0) node.msgcounter = currentCount - 1;
+        }, WindowInMilliSec);
+      }
 
-let currentCount = node.msgcounter || 0;
+      let currentCount = node.msgcounter || 0;
 
-if (currentCount < NoOfMsg) {
-    node.msgcounter = currentCount + 1;
-    addTimeout();
-    if (AddCurrentCount) msg.CurrentCount = currentCount + 1;
-    node.send([msg, null]);
-} else {
-    if (AddCurrentCount) msg.CurrentCount = currentCount;
-    node.send([null, msg]);
-}
-
-
-
-        });
-    }
-    RED.nodes.registerType("rate-limit",RateLimitNode);
-}
+      if (currentCount < NoOfMsg) {
+        node.msgcounter = currentCount + 1;
+        addTimeout();
+        if (AddCurrentCount) msg.CurrentCount = currentCount + 1;
+        node.send([msg, null]);
+      } else {
+        if (AddCurrentCount) msg.CurrentCount = currentCount;
+        node.send([null, msg]);
+      }
+    });
+  }
+  RED.nodes.registerType("rate-limit", RateLimitNode);
+};
